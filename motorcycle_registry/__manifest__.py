bb9c6ffe19e0d3a053d123523a0b34854057627f
GabrielRCL <gabriel-lucas@outlook.com.br>
2024-12-09 00:07:42 +0000

    [ADD] New Models

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': 'Motorcycle Registry This Module is used do keep track of the Motorcycle Registration and Onwership of each motorcycle of the brand',
+    'author': 'GabrielRCL',
+    'license': 'LGPL-3',
+    'version': '0.0.1',
+    'category': 'Kawiil/Custom Modules',
+    'website': 'www.github.com/GabrielRCL/odoo-training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
---
+++ motorcycle_registry/models/motorcycleregistry.py
@@ -0,0 +1,15 @@
+from odoo import fields,models
+
+class MotorcycleRegistry(models.Model):
+    _name = "motorcycle.registry"
+    _description = "Motorcycle Registry"
+    _rec_name = "registry_number"
+
+    certificate_title = fields.Binary(string="Certificate Title")
+    current_mileage = fields.Float()
+    date_registry = fields.Date(index=True)
+    first_name = fields.Char(string="First Name", required=True)
+    lasts_name = fields.Char(string="Last Name", required=True)
+    license_plate = fields.Char(string="license_plate")
+    registry_number = fields.Char(string="registry_number", required=True)
+    vin = fields.Char(string="VIN")
